0111d51045e0fc655be9512ccb6794098b916b58
rumsbooth baddam <rbaddam@chicagobooth.edu>
2023-04-13 14:15:08 +0000

    Unlucky Page Created

--- app/controllers/numbers_controller.rb
+++ app/controllers/numbers_controller.rb
@@ -10,4 +10,16 @@ class NumbersController < ApplicationController
 
     render({ :template => "lottery_stuff/woohoo.html.erb"})
   end 
+
+  
+  def losers
+    @unlucky = Array.new
+
+    5.times do
+      numbers = rand(1..100)
+      @unlucky.push(numbers)
+    end
+    render({ :template => "lottery_stuff/boohoo.html"})
+  end
+
 end
